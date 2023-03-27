054230592d5a8935bc0846de20ced6ca77bd8b0c
pnemani0193 <pardhasaradhisastry.nemani@valpo.edu>
2023-03-27 16:38:01 +0000

    first commit

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo $1 > token-file1
 gcloud config set project pn-project-374416
-gcloud compute instances create pn-instance-1 --access-token-file=token-file1 --project=pn-project-374416 --zone=us-central1-a --network-interface=subnet=pn-subnet1,no-address --tags=web-server,http-server,https-server
+gcloud compute instances create pn-instance-1 --access-token-file=token-file1 --zone=us-central1-a --network-interface=subnet=pn-subnet1,no-address --tags=web-server,http-server,https-server
